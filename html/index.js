314f9b49ffd9c9401e31697b1586661e8df7ea13
mabel <mabelare293@gmail.com>
2024-11-09 22:57:16 +0100

    all done

--- html/index.js
+++
@@ -1,5 +0,0 @@
-function toggleMenu() {
-  const overlayMenu = document.getElementById("overlayMenu");
-  overlayMenu.style.display =
-    overlayMenu.style.display === "block" ? "none" : "block";
-}
